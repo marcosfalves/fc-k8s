6f2405d108cbc5d19fb810f59f1534e1a79ca97b
Marcos F. Alves <marcosf_alves@outlook.com>
2023-08-22 07:26:11 -0300

    Injetando ConfigMap na aplicação

--- server.go
+++ server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8081", nil)
 }
@@ -15,4 +17,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/go/myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file:", err)
+	}
+	fmt.Fprintf(w, "My Family: %s.", string(data))
 }
